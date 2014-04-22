9e6db312822193ee33d2bb2ac231e26ea00844cd
Andrew Dupont <prototype@andrewdupont.net>
2014-04-21 19:22:43 -0500

    Update the HTML and JavaScript grammars to take advantage of new features.

--- languages/html.js
+++ languages/html.js
@@ -1,28 +1,49 @@
-Fluorescence.addLanguage('html', {
-	closingTag: {
-	  pattern: /(\/?(?:>|&gt;))/,
-	  replacement: "<span class=\"tag\">#{1}</span>"
-	},
-	
-	comment: {
-		pattern: /&lt;!\s*(--([^-]|[\r\n]|-[^-])*--\s*)&gt;/
-	},
-	
-	tag:  {
-	  pattern: /((?:<|&lt;)\/?)([a-zA-Z0-9:]+\s?)/,  
-		replacement: "<span class=\"#{0}\">#{1}#{2}</span>"
-	},
+(function() {
+  
+  var ATTRIBUTES_GRAMMAR = {
+    string: {
+      pattern: (/('[^']*[^\\]'|"[^"]*[^\\]")/)
+    },
 
-	string: {
-    pattern: /('[^']*[^\\]'|"[^"]*[^\\]")/
-	},
-
-	attribute: {
-		pattern: /\b([a-zA-Z-:]+)(=)/,
-		replacement: "<span class=\"#{0}\">#{1}</span>#{2}"
-	},
-	
-	doctype: {
-		pattern: /&lt;!DOCTYPE([^&]|&[^g]|&g[^t])*&gt;/
-	} 	
-});
+    attribute: {
+      pattern: (/\b([a-zA-Z-:]+)(=)/),
+      replacement: "<span class='attribute-with-equals-sign'><span class='#{0}'>#{1}</span><span class='punctuation'>#{2}</span></span>"
+    }
+  };
+  
+  var HTML_GRAMMAR = {
+    doctype: {
+      pattern: (/&lt;!DOCTYPE([^&]|&[^g]|&g[^t])*&gt;/)
+    },
+    
+    'embedded javascript-embedded': {
+      pattern: (/(&lt;)(script|SCRIPT)(\s+.*?)?(&gt;)([\s\S]*?)(&lt;\/)(script)(&gt;)/),
+      replacement: "<span class='element'><span class='punctuation'>#{1}</span><span class='tag'>#{2}</span>#{3}<span class='punctuation'>#{4}</span></span>#{5}<span class='element closing-element'><span class='punctuation'>#{6}</span><span class='tag'>#{7}</span><span class='punctuation'>#{8}</span></span>",
+      
+      beforeCallback: function(r) {
+        r[3] = r[3] ? Fluorescence.parse(r[3], ATTRIBUTES_GRAMMAR) : "";
+        r[5] = Fluorescence.parse(r[5], 'javascript');
+      }
+    },
+    
+    tag: {
+      pattern: (/((?:<|&lt;))([a-zA-Z0-9:]+\s*)(.*?)(\/)?(&gt;)/),
+      replacement: "<span class='element'><span class='punctuation'>#{1}</span><span class='tag'>#{2}</span>#{3}<span class='punctuation'>#{4}#{5}</span></span>",
+      
+      beforeCallback: function(r) {
+        r[3] = Fluorescence.parse(r[3], ATTRIBUTES_GRAMMAR);
+      }
+    },
+    
+    'tag closing-tag': {
+      pattern: (/(&lt;\/)([a-zA-Z0-9:]+)(&gt;)/),
+      replacement: "<span class='element closing-element'><span class='punctuation'>#{1}</span><span class='tag'>#{2}</span><span class='punctuation'>#{3}</span></span>"
+    },
+    
+    comment: {
+      pattern: (/&lt;!\s*(--([^-]|[\r\n]|-[^-])*--\s*)&gt;/)
+    }
+  };
+  
+  Fluorescence.addLanguage('html', HTML_GRAMMAR);
+})();
--- languages/javascript.js
+++ languages/javascript.js
@@ -1,47 +1,92 @@
-Fluorescence.addLanguage('javascript', 
-{
-  fun: {
-    pattern: /\b(function)\s+([a-zA-Z_$]\w*)?\s*\((.*?)\)/,
-    replacement: "<span class='function'>#{1}</span> <span class='entity'>#{2}</span>(<span class='parameter'>#{3}</span>)"
-  },
-  
-  fun2: {
-    pattern: /\b([a-zA-Z_?\.$]+\w*)\s+=\s+\b(function)?\s*\((.*?)\)/,
-    replacement: "<span class='entity'>#{1}</span> = <span class='function'>#{2}</span>(<span class='parameter'>#{3}</span>)"
-  },
-  
-  fun3: {
-    pattern: /\b(function)\s*\((.*?)\)/,
-    replacement: "<span class='function'>#{1}</span>(<span class='parameter'>#{2}</span>)"
-  },
-  
-  variable: {
-    pattern: /\bvar\s+([A-Za-z_$]+?)\b/,
-    replacement: "var <span class='#{0}'>#{1}</span>"
-  },
-  
-  string: {
-    pattern: /'[^']*[^\\]'|"[^"]*[^\\]"/
-  },
-  
-  comment: {
-    pattern: /(\/\/[^\n]*\n)|(\/\*[^*]*\*+([^\/][^*]*\*+)*\/)/
-  },
-  
-  control: {
-    pattern: /\b(break|case|catch|continue|default|do|else|finally|for|goto|if|import|package|return|switch|throw|try|while|with)\b/
-  },
-  
-  constant: {
-    pattern: /\b(false|null|super|this|true)\b/
-  },
-  
-  property: {
-    pattern: /\b([A-Za-z_$]+?):\s/,
-    replacement: "<span class='#{0}'>#{1}</span>: "
-  },
-  
-  number: {
-    pattern: /\b((0(x|X)[0-9a-fA-F]+)|([0-9]+(\.[0-9]+)?))\b/
+(function() {
+  function highlightParams(text) {
+    function wrapParameter(text) {
+      return "<span class='variable parameter'>" + text + "</span>";
+    }
+    
+    return text.split(/,\s*/).map(wrapParameter).join(', ');
   }
-});
+  
+  var ESCAPES_GRAMMAR = {
+    escape: {
+      pattern: (/\\./)
+    }
+  };
+  
+  Fluorescence.addLanguage('javascript',
+  {
+    keyword: {
+      pattern: (/new(?=\s[A-Za-z_$])/),
+      replacement: "<span class='#{0}'>new</span>"
+    },
+    
+    variable: {
+      pattern: (/\bvar\s+([A-Za-z_$]+?)\b/),
+      replacement: "var <span class='#{0}'>#{1}</span>"
+    },
+  
+    fun: {
+      pattern: ((/\b(function)\s+([a-zA-Z_$]\w*)?\s*(\()(.*?)(\))/)),
+      replacement: "<span class='function'>#{1}</span> <span class='entity'>#{2}</span>#{3}#{4}#{5}",
+    
+      beforeCallback: function(r, highlight) {
+        r[4] = highlightParams(r[4]);
+        return r;
+      }
+    },
+  
+    fun2: {
+      pattern: (/\b([a-zA-Z_?\.$]+\w*)\s+=\s+\b(function)?\s*\((.*?)\)/),
+      replacement: "<span class='entity'>#{1}</span> = <span class='function'>#{2}</span>(<span class='parameter'>#{3}</span>)"
+    },
+  
+    fun3: {
+      pattern: (/\b(function)\s*(\()(.*?)(\))/),
+      replacement: "<span class='function'>#{1}</span>#{2}#{3}#{4}",
+      
+      beforeCallback: function(r, highlight) {
+        r[3] = highlightParams(r[3]);
+        return r;
+      }
+    },
+  
+    'single-quoted string': {
+      pattern: (/(')([^']*?)(')/),
+      replacement: "<span class='#{0}'>#{1}#{2}#{3}</span>",
+      beforeCallback: function(r) {
+        r[2] = Fluorescence.parse(r[2], ESCAPES_GRAMMAR);
+        return r;
+      }
+    },
+    
+    'double-quoted string': {
+      pattern: (/(")(.*?[^\\])(")/),
+      replacement: "<span class='#{0}'>#{1}#{2}#{3}</span>",
+      beforeCallback: function(r) {
+        r[2] = Fluorescence.parse(r[2], ESCAPES_GRAMMAR);
+        return r;
+      }
+    },
+  
+    comment: {
+      pattern: (/(\/\/[^\n]*\n)|(\/\*[^*]*\*+([^\/][^*]*\*+)*\/)/)
+    },
+  
+    control: {
+      pattern: (/\b(break|case|catch|continue|default|do|else|finally|for|goto|if|import|package|return|switch|throw|try|while|with)\b/)
+    },
+  
+    constant: {
+      pattern: (/\b(false|null|super|this|true)\b/)
+    },
+  
+    property: {
+      pattern: (/\b([A-Za-z_$]+?):\s/),
+      replacement: "<span class='#{0}'>#{1}</span>: "
+    },
+  
+    number: {
+      pattern: (/\b((0(x|X)[0-9a-fA-F]+)|([0-9]+(\.[0-9]+)?))\b/)
+    }
+  });
+})();
